453cf2414ae474497128b15269e53c37aa9d91b0
Maxkaizo <jlmartinezol@outlook.com>
2025-07-27 12:59:16 -0600

    test drata drift report 	modified:   Pipfile 	modified:   Pipfile.lock 	new file:   lab/monitor_test_evidently.ipynb 	modified:   mlflow/server.sh 	new file:   monitor/reports/2025-06-data-drift-report.html 	new file:   monitor/reports/test-drift-report.evidently.html 	modified:   orchestration/tasks/monitor_data_drift.py

--- mlflow/server.sh
+++ mlflow/server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /home/maxkaizo/milk_price_prediction/mlflow
+
 mlflow server \
   --backend-store-uri=sqlite:///mlflow.db \
   --default-artifact-root=s3://mlflow-models-milk-price-dev \
--- orchestration/tasks/monitor_data_drift.py
+++ orchestration/tasks/monitor_data_drift.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from prefect import task
-from evidently.report import Report
-from evidently.metrics import DataDriftPreset
+from evidently import Report
+from evidently.presets import DataDriftPreset
 import pandas as pd
 
 
@@ -14,42 +14,22 @@ def monitor_data_drift(
     year: int,
     month: int,
 ) -> str:
-    """
-    Generates a data drift report using Evidently, comparing:
-    - Reference: all training data (months -13 to -2)
-    - Current: validation data (month -1)
-
-    The report is saved as HTML in the `monitor/reports/` folder.
-
-    Args:
-        X_ref_dicts (list): List of dicts for reference data (training).
-        y_ref (list): List of prices for reference data.
-        X_cur_dicts (list): List of dicts for current data (validation).
-        y_cur (list): List of prices for current data.
-        year (int): Execution year.
-        month (int): Execution month.
-
-    Returns:
-        str: Path to the generated HTML report.
-    """
-
-    # Convert to DataFrames
     df_ref = pd.DataFrame(X_ref_dicts)
     df_ref["Precio"] = y_ref
 
     df_cur = pd.DataFrame(X_cur_dicts)
     df_cur["Precio"] = y_cur
 
-    # Create Evidently report
     report = Report(metrics=[DataDriftPreset()])
     report.run(reference_data=df_ref, current_data=df_cur)
 
-    # Define output path
+    # Carpeta destino
     output_dir = Path("monitor/reports")
     output_dir.mkdir(parents=True, exist_ok=True)
-    output_path = output_dir / f"{year}-{month:02d}-data-drift-report.html"
 
+    # Guardamos el reporte en su formato nativo (.evidently.html)
+    output_path = output_dir / f"{year}-{month:02d}-data-drift-report.evidently.html"
     report.save_html(str(output_path))
-    print(f"📊 Data drift report saved to: {output_path}")
 
+    print(f"📊 Data drift report saved to: {output_path}")
     return str(output_path)
